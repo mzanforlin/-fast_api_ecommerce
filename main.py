08464b54b8726d1cdd1986e2fc8a89e40eba9536
Matheus Zanforlin <zanforlin14@gmail.com>
2025-04-28 17:33:53 -0300

    vendas.py add

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 from fastapi import FastAPI
+from vendas import vendas
 
 app = FastAPI()
 
-vendas = {
-
-    1: {"item": "lata", "preco_unitario": 4, "quantidade": 5},
-    2: {"item": "garrafa 2L", "preco_unitario": 15, "quantidade": 5},
-    3: {"item": "garrafa 750ml", "preco_unitario": 10, "quantidade": 5},
-    4: {"item": "lata mini", "preco_unitario": 2, "quantidade": 5}
-} 
 
 @app.get("/")
 def home():
